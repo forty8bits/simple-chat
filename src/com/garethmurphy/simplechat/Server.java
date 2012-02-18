c2a16019e280980c5832fc64ed4144b3e1cd0619
Gareth Murphy <gareth.cpm@gmail.com>
2012-02-18 00:26:40 +0000

    Huge refactor of Server class, much nicer

--- src/com/garethmurphy/simplechat/Server.java
+++ src/com/garethmurphy/simplechat/Server.java
@@ -7,87 +7,113 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
-
-	private int port = 5000;
-
-	private ArrayList<Socket> connections = new ArrayList<Socket>();
-	private ArrayList<Thread> threads = new ArrayList<Thread>();
+	private ServerSocket socket;
+	private List<Thread> clientListenThreads = new ArrayList<Thread>();
+	private List<Socket> connectionSockets = new ArrayList<Socket>();
 
 	public static void main(String[] args) {
 		new Server().start();
 	}
 	
 	public void start() {
-		try {
-			ServerSocket serverSocket = new ServerSocket(port);
-			int currentConnectionIndex = 0;
-			
-			while(true) {	
-				Socket clientSocket = serverSocket.accept();
-				connections.add(clientSocket);
-				
-				threads.add(new Thread(new ClientListener(
-						currentConnectionIndex, clientSocket)));
-				
-				threads.get(currentConnectionIndex).start();
-				
-				currentConnectionIndex++;
-				
-				System.out.println("Connected to: " 
-						+ clientSocket.getInetAddress().toString());
-			}
-		} catch(IOException ex) {
-			ex.printStackTrace();
+		startListeningOnPort(5000);
+		while(true) {
+			Socket clientSocket = waitAndReceiveConnection();
+			connectionSockets.add(clientSocket);
+			setupClientListenThread(clientListenThreads.size(), clientSocket);
 		}
+	}
+	
+	private void startListeningOnPort(int port) {
+		try {
+			socket = new ServerSocket(port);
+		} catch(IOException e) {
+			System.err.println("Couldn't open server socket.");
+			e.printStackTrace();
+		}
+	}
+	
+	private Socket waitAndReceiveConnection() {
+		try {
+			Socket clientSocket = socket.accept();
+			System.out.println("Connected to " + clientSocket.getInetAddress().toString());
+			return clientSocket;
+		} catch(IOException e) {
+			System.err.println("Error on receiving incoming connection.");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private void setupClientListenThread(int index, Socket clientSocket) {
+		ClientListener clientListener = new ClientListener(index, clientSocket);
+		Thread clientThread = new Thread(clientListener);
+		clientListenThreads.add(clientThread);
+		clientThread.start();
 	}
 	
 	class ClientListener implements Runnable {
-		
+		private int indexInLists;
+		private Socket clientSocket;
 		private BufferedReader inStream;
-		private String name;
+		private String userName;
 		
-		public ClientListener(int index, Socket socket) {
-			try {
-				
-				InputStreamReader rawInStream = new InputStreamReader(
-						socket.getInputStream());
-				
-				inStream = new BufferedReader(rawInStream);
-			} catch(IOException ex) {
-				ex.printStackTrace();
-			}
-			
+		public ClientListener(int index, Socket clientSocket) {
+			indexInLists = index;
+			this.clientSocket = clientSocket;
+			setupInStream();			
 			// TODO: Give proper option to set name
-			name = "User " + index;
+			userName = "User " + index;
+		}
+		
+		private void setupInStream() {
+			try {
+				InputStreamReader rawInStream = new InputStreamReader(clientSocket.getInputStream());
+				inStream = new BufferedReader(rawInStream);
+			} catch(IOException e) {
+				System.err.println("Problem getting client incoming stream.");
+				e.printStackTrace();
+			}
 		}
 		
 		public void run() {
-			String msg;
+			listenForAndForwardMsgs();
+		}
+		
+		private void listenForAndForwardMsgs() {
 			try {
+				String msg;
 				while((msg = inStream.readLine()) != null) {
 					// TODO: Implement empty message checking
-					sendMessage(name, msg);
+					sendMessage(userName, msg);
 				}
-			} catch(IOException ex) {
-				ex.printStackTrace();
+			} catch(IOException e) {
+				System.err.println("Problem on message receive/forward.");
+				e.printStackTrace();
 			}
 		}
 	}
 	
-	public void sendMessage(String name, String msg) {
+	public void sendMessage(String userName, String msg) {
 		
-		for (Socket s : connections) {
-			try {
-				PrintWriter writer = new PrintWriter(s.getOutputStream());
-				writer.println(name + ": " + msg + "\n");
-				
-				// The PrintWriter is buffered, so needs to be flushed 
-				writer.flush();
-			} catch(IOException ex) {
-				ex.printStackTrace();
-			}
+		for (Socket s : connectionSockets) {
+			PrintWriter socketOutput = getSocketOutWriter(s);
+			socketOutput.println(userName + ": " + msg + "\n"); 
+			socketOutput.flush();
 		}
+	}
+	
+	private PrintWriter getSocketOutWriter(Socket socket) {
+		try {
+			PrintWriter socketOutput = new PrintWriter(socket.getOutputStream());
+			return socketOutput;
+		} catch(IOException e) {
+			System.err.println("Error on getting socket OutputStream.");
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
